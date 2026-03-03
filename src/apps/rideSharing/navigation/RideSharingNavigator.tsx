790d816eed0ab665f601f121616bd15981533589
faizan tayyab <faizantayyab37@gmail.com>
2026-03-03 13:40:45 +0500

    feat: Add Driver Profile screen, refactor Home screen, and update navigation and localization.

--- src/apps/rideSharing/navigation/RideSharingNavigator.tsx
+++ src/apps/rideSharing/navigation/RideSharingNavigator.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import RideSharingHomeScreen from '../screens/HomeScreen';
-import RideDetails from '../screens/RideDetails';
+import RideSharingHomeScreen from '../screens/home/HomeScreen';
+import RideDetails from '../screens/rideDetails/RideDetails';
+import DriverProfileScreen from '../screens/driverProfile/DriverProfileScreen';
 import { useTranslation } from 'react-i18next';
 import QueryProvider from '../providers/QueryProvider';
 
@@ -14,6 +15,11 @@ export default function RideSharingNavigator() {
       <Stack.Navigator>
         <Stack.Screen name="RideSharingHome" component={RideSharingHomeScreen} options={{ title: t('header_title') }} />
         <Stack.Screen name="RideDetails" component={RideDetails} options={{ title: t('details_title') }} />
+        <Stack.Screen
+          name="DriverProfile"
+          component={DriverProfileScreen}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </QueryProvider>
   );
