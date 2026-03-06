c5a0b9da93a451e0f917d7442d59251daf9ce693
faizan tayyab <faizantayyab37@gmail.com>
2026-03-06 10:52:30 +0500

    fixed backgroud color of icon navigation back in screen header

--- src/general/components/ScreenHeader.tsx
+++ src/general/components/ScreenHeader.tsx
@@ -82,7 +82,7 @@ export default function ScreenHeader({
               accessibilityLabel="Go back"
               hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
             >
-              <View style={[styles.backButton, { backgroundColor: colors.background }]}>
+              <View style={[styles.backButton, { backgroundColor: colors.backgroundTertiary }]}>
                 <Image
                   source={require('../assets/images/backButton.png')}
                   style={styles.backIcon}
