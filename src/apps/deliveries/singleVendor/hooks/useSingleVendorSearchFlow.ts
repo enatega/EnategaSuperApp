8e0185ffa55c019b440ae0d43dd1120f25a70d60
Kashif Shehzad Baloch <kashif.shehzad.725@gmail.com>
2026-04-08 16:32:45 +0500

    added address from useAddresses hook in the Search flow being used in chain, multi and single vendor apps

--- src/apps/deliveries/singleVendor/hooks/useSingleVendorSearchFlow.ts
+++ src/apps/deliveries/singleVendor/hooks/useSingleVendorSearchFlow.ts
@@ -1,17 +1,7 @@
-import { useNavigation } from '@react-navigation/native';
-import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import useDeliverySearchFlow from '../../hooks/searchFlow/useDeliverySearchFlow';
-import type { DeliveriesStackParamList } from '../../navigation/types';
-
-type NavigationProp = NativeStackNavigationProp<DeliveriesStackParamList>;
 
 export default function useSingleVendorSearchFlow() {
-  const navigation = useNavigation<NavigationProp>();
-
   return useDeliverySearchFlow({
     searchStores: false,
-    onAddressPress: () => {
-      navigation.navigate('AddressSearch', { origin: 'single-vendor-home' });
-    },
   });
 }
